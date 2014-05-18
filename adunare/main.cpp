9f394e764090ec0f72f5531dcdf4945138ab067e
Gergely David <gergelydavid97@gmail.com>
2014-05-18 22:29:45 +0300

    Update

---
+++ adunare/main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+
+using namespace std ;
+ifstream cin("adunare.in") ;
+ofstream cout("adunare.out") ;
+int A, B ;
+
+int main()
+{
+   s cin >> A >> B ;
+    cout << A + B << ' ';
+
+
+    cin.close();
+    cout.close();
+    return 0 ;
+}
